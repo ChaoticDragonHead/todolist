e1a5611b3b5fadcb8bdb43883c68f4b71552f1bf
ChaoticDragonHead <136110505+ChaoticDragonHead@users.noreply.github.com>
2025-06-15 14:30:18 -0700

    Added custom item functionality

--- js/additem.js
+++ js/additem.js
@@ -1,18 +1,31 @@
-function addItem(){
+function addItem(parent){
     console.log("Add Item Function triggered")
 
     let newItem = document.createElement('li');
-    let checked = document.createElement('input');
+    let inp = document.createElement('input');
     let text = document.createElement('span');
 
-    checked.setAttribute('type','checkbox');
-    checked.setAttribute('class', 'item');
+    //checked.setAttribute('type','checkbox');
+    inp.setAttribute('class', 'item');
 
-    text.textContent = 'New Item';
+    //text.textContent = 'New Item';
 
-    list.appendChild(newItem);
-    newItem.appendChild(checked);
+    parent.appendChild(newItem);
+    newItem.appendChild(inp);
     newItem.appendChild(text);
+
+    newItem.addEventListener('keydown', function(event){
+        if(event.key === 'Enter'){
+            addText(inp, text);
+        }
+    })
+}
+
+function addText(chil1, chil2){
+    console.log("Add text function triggered")
+
+    chil2.textContent = chil1.value;
+    chil1.setAttribute('type', 'checkbox');
 }
 
 console.log("Successfully added script to page")
@@ -20,4 +33,6 @@ console.log("Successfully added script to page")
 let but1 = document.getElementById('butt1');
 const list = document.getElementById('itList');
 
-but1.addEventListener('click', addItem);
+but1.addEventListener('click', () => {
+    addItem(list);
+});
