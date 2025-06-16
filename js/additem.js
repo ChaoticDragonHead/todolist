fcf6589e0a86873fcc9be9ecc74be5af77125d5a
ChaoticDragonHead <136110505+ChaoticDragonHead@users.noreply.github.com>
2025-06-15 17:59:21 -0700

    Added delete function as well as changed the introduction text on the home page to better explain how to use the app. Set the list to display the most recently added item at the top - will add a toggle eventually.

--- js/additem.js
+++ js/additem.js
@@ -5,6 +5,13 @@ function addItem(parent){
     let inp = document.createElement('input');
     let text = document.createElement('span');
 
+    let rem = document.createElement('button');
+    rem.textContent = "Delete Item"
+    rem.addEventListener('click', () =>{
+        removeItem(newItem);
+    });
+    rem.setAttribute('class', 'delete_hid');
+
     //checked.setAttribute('type','checkbox');
     inp.setAttribute('class', 'item');
 
@@ -13,25 +20,38 @@ function addItem(parent){
     parent.appendChild(newItem);
     newItem.appendChild(inp);
     newItem.appendChild(text);
+    newItem.appendChild(rem);
 
     newItem.addEventListener('keydown', function(event){
         if(event.key === 'Enter'){
-            addText(inp, text);
+            addText(inp, text, rem);
         }
-    })
+    });
 }
 
-function addText(chil1, chil2){
+function addText(chil1, chil2, butt){
     console.log("Add text function triggered")
 
     chil2.textContent = chil1.value;
     chil1.setAttribute('type', 'checkbox');
+    butt.setAttribute('class', 'delete');
+}
+
+function removeItem(parent){
+    console.log("Remove Item Function triggered")
+
+    /*
+    This function is attached to the rem button. Rem is a child of the li newItem. newItem and all of its children must be deleted
+    */
+   parent.remove();
 }
 
 console.log("Successfully added script to page")
 
 let but1 = document.getElementById('butt1');
 const list = document.getElementById('itList');
+
+addItem(list);
 
 but1.addEventListener('click', () => {
     addItem(list);
